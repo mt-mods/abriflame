e6fc59dce4a0e96449c730e07a6fc50ee34aecc8
Sheriff_U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-08-28 17:53:18 -0500

    Standardize (#8)
    * Add .cdb.json
    
    * Add/Update workflows and .luacheckrc
    
    * Update mod.conf
    
    * Optimize PNGs
    
    * minetest --> core
    
    - Format with StyLua
    
    * Add translation support
    
    * Improve README.md

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-local modpath = minetest.get_modpath("abriflame").. DIR_DELIM
+local modpath = core.get_modpath("abriflame") .. DIR_DELIM
 
 abriflame = {}
 
-dofile(modpath.."nodes.lua")
+dofile(modpath .. "nodes.lua")
 
 abriflame.init = {}
